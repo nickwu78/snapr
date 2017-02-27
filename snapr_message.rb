e569ef8d792631ee6f2696ba6058540272e2cd05
Nick Wu <nickwu78@gmail.com>
2017-02-28 10:05:08 +1100

    added file size limit logic

--- snapr_message.rb
+++ snapr_message.rb
@@ -1,7 +1,7 @@
 require "zlib"
 
 class SnaprMessage
-
+	MAX_SIZE = 66560
 	attr_accessor :message
 
 	def initialize(message)
@@ -9,7 +9,14 @@ class SnaprMessage
 	end
 
 	def encode_to_ascii
-		Zlib::Deflate.deflate(@message)
+		data_compressed = Zlib::Deflate.deflate(@message)
+			if data_compressed.size > MAX_SIZE
+				puts "File size exceeds maximum"
+				exit
+			else
+				puts "Compressed size: #{data_compressed.size}"
+				return data_compressed
+			end
 	end
 
 	def self.decode_from_ascii(encoded_string)
